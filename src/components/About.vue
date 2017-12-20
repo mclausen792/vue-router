7a0fb922b48e5c2437c518e916fe90fd95e3c827
Makayla Clausen <mclausen792@gmail.com>
2017-12-20 11:13:28 -0700

    Adding files.

---
+++ src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="about">
+  <h2> About Me </h2>
+  I am a JavaScript Developer with a passion for design. I stumbled upon code when I was working in my previous career in customer service and never turned back. I am excited about the opportunities that web development has opened up for me and look forward to a career that is both creative and intellectual.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about'
+}
+</script>
+<!-- styling for the component -->
+<style>
+#about {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>Makayla Clausen</h1>
+    <h2>{{ msg }}</h2>
+    <img src = "../assets/me.jpg" />
+    <h2>My Projects</h2>
+    <ul>
+      <li><a href="https://my-react-2b03f.firebaseapp.com/" target="_blank">React Inbox</a></li>
+      <li><a href="https://first-64-project.firebaseapp.com/" target="_blank">Inspire Me</a></li>
+      <li><a href="https://galvanizegamers.herokuapp.com/" target="_blank">Galvanize Games</a></li>
+      <li><a href="https://rocky-dawn-23084.herokuapp.com/" target="_blank">Galvanize Reads</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to my Web Page!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+img {
+width: 90px;
+border-radius: 50%;
+}
+</style>
